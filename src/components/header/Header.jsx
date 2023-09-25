5bafd16447ed1476a57f60a481720c996cd3f7c1
abusayed0 <abusayed48876@gmail.com>
2023-09-26 02:24:59 +0600

    added header and navigation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,25 @@
-
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/Logo.png'
+import './Header.css'
 const Header = () => {
     return (
-        <nav>
-            <h2>This is Header component</h2>
+        <nav className="border flex  items-center gap-5 flex-col md:flex-row md:justify-between">
+            <div>
+                <img className="h-[50px] w-[150px] md:h-[70px] md:w-[250px] object-contain" src={logo} alt="" />
+            </div>
+            <div>
+                <ul className="flex items-center gap-10">
+                    <li>
+                        <NavLink to="/" className="text-lg">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/donation" className="text-lg">Donation</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/statistics" className="text-lg">Statistics</NavLink>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
