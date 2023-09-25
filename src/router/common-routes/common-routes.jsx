0760b571c6b585801ac73e6b92f0d0d6d9a3e6e9
abusayed0 <abusayed48876@gmail.com>
2023-09-26 01:09:07 +0600

    added layout

---
+++ src/router/common-routes/common-routes.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootElement from "../../layouts/RootElement";
+import Home from "../../pages/home/Home";
+import Donation from "../../pages/donation/Donation";
+import Statistics from "../../pages/statistics/Statistics";
+
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <RootElement/>,
+        children : [
+            {
+              path : "/", 
+              element : <Home/> 
+            },
+            {
+                path : "/donation",
+                element : <Donation/>
+            },
+            {
+                path : "/statistics",
+                element : <Statistics/>
+            }
+        ]
+    }
+]);
+export default router;
